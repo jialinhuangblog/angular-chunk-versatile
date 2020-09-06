dfe150e32ab509b5a5abba3d1a0c6a2de347efff
jia0_nn <linao264590000@gmail.com>
2020-09-06 21:28:06 +0800

    list animation

--- src/app/components/tldr/tldr.component.ts
+++ src/app/components/tldr/tldr.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { ChunkService } from 'src/app/services/chunk.service';
 import { Chunk } from 'src/app/models/chunk';
+import { trigger, style, transition, animate, query, stagger } from '@angular/animations';
+
+
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(':enter',
+      [style({ opacity: 0 }), stagger('100ms', animate('600ms ease-out', style({ opacity: 1 })))],
+      { optional: true }
+    ),
+    query(':leave',
+      animate('200ms', style({ opacity: 0 })),
+      { optional: true}
+    )
+  ])
+]);
 
 @Component({
   selector: 'app-tldr',
   templateUrl: './tldr.component.html',
-  styleUrls: ['./tldr.component.scss']
+  styleUrls: ['./tldr.component.scss'],
+  animations: [
+    listAnimation
+  ],
 })
-export class TLDRComponent implements OnInit {
-  chunks: Chunk[] = [];
+
+
+export class TLDRComponent implements OnInit, OnChanges {
+
   constructor(private chunkService: ChunkService) { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes')
+  }
+  visibility: Boolean = false;
+  chunks: Chunk[] = [];
 
   ngOnInit(): void {
     this.getChunks();
@@ -23,9 +49,7 @@ export class TLDRComponent implements OnInit {
         this.chunks = []
         actions.forEach(action => {
           let item = action.payload.doc;
-          
           var data = item.data();
-          console.log(data)
           data['$key'] = item.id;
           this.chunks.push(data as Chunk);
         })
